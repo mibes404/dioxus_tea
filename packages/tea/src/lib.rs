be568774e0cc6af6a760842373c29ae589e00ae9
Marcel <mibes@avaya.com>
2025-06-24 15:29:21 +0200

    enhance: TeaModel with documentation

--- packages/tea/src/lib.rs
+++ packages/tea/src/lib.rs
@@ -9,12 +9,14 @@ use dioxus::prelude::{
 };
 use futures_util::StreamExt;
 
+/// Trait representing a TEA model in Dioxus.
 pub trait TeaModel: 'static + Default + Clone + PartialEq {
     type Action;
 
     fn update(action: Self::Action, writer: Write<Self>);
 }
 
+/// A signal that holds the state of a `TeaModel` and provides an internal coroutine for processing actions.
 #[derive(Clone, PartialEq)]
 pub struct TeaModelSignal<T: TeaModel> {
     inner: Signal<T>,
@@ -25,16 +27,19 @@ impl<T: TeaModel> Copy for TeaModelSignal<T> {}
 
 impl<T: TeaModel> TeaModelSignal<T> {
     #[must_use]
+    /// Returns a reference to the underlying signal for reading the model state.
     pub fn read(&self) -> ReadableRef<Signal<T>> {
         self.inner.read()
     }
 
+    /// Sends an action to the coroutine for processing.
     pub fn send(&self, action: T::Action) {
         self.co.send(action);
     }
 }
 
 #[must_use]
+/// Creates a new `TeaModelSignal` for the given `TeaModel`.
 pub fn use_tea_model<T: TeaModel>() -> TeaModelSignal<T> {
     let mut inner = use_signal(|| T::default());
 
